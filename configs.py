1a7de0944988112e31b3d656a924bb3ba5901256
Telugumacha <156811630+Telugumacha@users.noreply.github.com>
2024-02-11 13:27:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,13 +6,13 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "20389440"))
-    API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-    FSUB = getenv("FSUB", "VJ_Botz")
-    CHID = int(getenv("CHID", "-1001623633000"))
-    SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+    API_ID = int(getenv("API_ID", "25255983"))
+    API_HASH = getenv("API_HASH", "5d3616ef09b67e8cf45a96a11fa57b22")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6772654586:AAGG1HqLwow5OxiHEAl3RraQLX2Fx5V6Xqg")
+    FSUB = getenv("FSUB", "Telugumacha_updates")
+    CHID = int(getenv("CHID", "-1002080428958"))
+    SUDO = list(map(int, getenv("SUDO", "5136811766").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://mecesa9366:Olt6lwGCkBiCDVKz@cluster0.dkbhwox.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
 
